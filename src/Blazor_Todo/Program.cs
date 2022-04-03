3477a5aeb19cee644c63715ce3584299bf22ecc9
Toni Le <toni.le@outlook.de>
2022-04-03 18:57:09 +0200

    Re-commit Blazor project

--- src/Blazor_Todo/Program.cs
+++ src/Blazor_Todo/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using src;
+using Blazor_Todo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
